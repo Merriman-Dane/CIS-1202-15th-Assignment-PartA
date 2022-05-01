4e76ea33fafd8ba64c1fbd6120a3c643210db288
Dane Merriman <merriman.dane02@gmail.com>
2022-05-01 19:52:25 -0400

    Created character function

--- Source.cpp
+++ Source.cpp
@@ -38,3 +38,23 @@ class invalidRangeException
 			return i;
 		}
 };
+
+char character(char start, int offset)
+{
+	// If start is NOT between 'a' and 'z' or 'A' and 'Z'
+	if (!((start >= 'a' && start <= 'z') || (start >= 'A' && start <= 'Z')))
+	{
+		throw invalidCharacterException(start);
+	}
+
+	char newChar = start + offset;
+
+	// If newChar is NOT between 'a' and 'z' or 'A' and 'Z'
+	// OR if offset is greater than or equal to 32 (to prevent looping from 'A' to 'a')
+	if (!((newChar >= 'a' && newChar <= 'z') || (newChar >= 'A' && newChar <= 'Z')) || offset >= 32)
+	{
+		throw invalidRangeException(offset);
+	}
+
+	return newChar;
+}
